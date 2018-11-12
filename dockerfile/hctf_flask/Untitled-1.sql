10d7dcf6cb211f626e11f2aa158649a5e2fa52ff
apple <491565693@qq.com>
2018-11-12 22:29:26 +0800

    HCTF2018_admin

--- dockerfile/hctf_flask/Untitled-1.sql
+++
@@ -1,5 +0,0 @@
-create table `user` (
-    `id` int (10) auto_increment, email varchar(120), 
-    `password_hash` varchar(128), 
-    `username` varchar(64) binary, 
-    primary key(`id`));
--- dockerfile/hctf_flask/run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-from app import app
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', 9999, debug=True)
